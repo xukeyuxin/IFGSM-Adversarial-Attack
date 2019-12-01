32e9d49c74d8880029b3726b2418a99d2d7944e4
Administrator <admin@example.com>
2019-12-01 18:52:45 +0800

    save

--- train.py
+++ train.py
@@ -490,7 +490,10 @@ class Classify(op_base):
         # tmp_noise = self.noise - lr * (finetune_grad + loss1_v)
         gradient_mix = tf.reshape(finetune_grad + loss1_grad, [299*299*3])
         gradient_mask_index = tf.math.top_k(gradient_mix, 100).indices
+        gradient_mask_index = tf.expand_dims(gradient_mask_index,axis = -1)
         gradient_mask_value = tf.math.top_k(gradient_mix, 100).values
+        print(gradient_mask_index.shape)
+        print(gradient_mask_value.shape)
         gradient_scatter = tf.scatter_nd(gradient_mask_index,gradient_mask_value, tf.zeros_like(gradient_mix))
         print('find update')
         print(gradient_scatter)
