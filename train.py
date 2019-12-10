4497eac0ad2f3005c113f3d1e8e69b3e64f7562f
Administrator <admin@example.com>
2019-12-10 20:07:54 +0800

    save

--- train.py
+++ train.py
@@ -739,13 +739,11 @@ class Classify(op_base):
             print('start attack %s' % _image_path)
             for i in range(0,501):
                 _image_content = np_random_process(_image_content)
-                print(_image_content.shape)
                 feed_dict = self.make_feed_dict(_image_content,target_input,label_input,mask,i)
                 _,write_image,_weight,_loss,_t_loss,_l_loss = self.sess.run([train_op,self.combine_images,self.loss_weight,self.total_loss,self.target_cross_entropy_resnet_tel,self.label_cross_entropy_resnet_tel],feed_dict = feed_dict)
-                print(_loss)
-                print(_t_loss)
-                print(_l_loss)
-                self.writer(str(i) + '--' + _image_path,_resize_image)
+                print('loss %s :' % _loss)
+                print('t_loss %s :' % _t_loss)
+                print('t_loss %s :' % _l_loss)
                 print('-----------finish %s' % i)
                 # if( _loss <= -120.):
                 #     self.writer(_image_path,write_image)
