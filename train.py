dd1fbffa0eb2ff5c84804d1d3f277b4779ff5da2
Administrator <admin@example.com>
2019-12-10 14:37:22 +0800

    save

--- train.py
+++ train.py
@@ -736,9 +736,8 @@ class Classify(op_base):
             print('start attack %s' % _image_path)
             for i in range(0,501):
                 feed_dict = self.make_feed_dict(_image_content,target_input,label_input,mask,i)
-                _,write_image,_weight,_loss,_t_loss = self.sess.run([train_op,self.combine_images,self.loss_weight,self.total_loss,self.target_cross_entropy_resnet_tel],feed_dict = feed_dict)
+                _,write_image,_weight,_loss,_t_loss = self.sess.run([train_op,self.combine_images,self.loss_weight,self.total_loss],feed_dict = feed_dict)
                 print(_loss)
-                print(_t_loss)
                 # if( _loss <= -120.):
                 #     self.writer(_image_path,write_image)
                 #     print('finish one attack  weight: %s with step: %s' %  (_weight,i))
