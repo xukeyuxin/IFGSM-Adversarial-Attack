b4cd2b2cb097dd3867f8e1d7e9bdb21e153ccccf
Administrator <admin@example.com>
2019-12-10 09:44:15 +0800

    sae

--- train.py
+++ train.py
@@ -725,7 +725,7 @@ class Classify(op_base):
             _image_content = np.expand_dims(_image_content ,0) # (1,299,299,3)
             mask = np.ones([1,299,299,1])
             print('start attack %s' % _image_path)
-            for i in range(0,51):
+            for i in range(0,200):
                 feed_dict = self.make_feed_dict(_image_content,target_input,label_input,mask,i)
                 _,write_image,_weight,_loss = self.sess.run([train_op,self.combine_images,self.loss_weight,self.total_loss],feed_dict = feed_dict)
                 print(_loss)
